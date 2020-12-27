7ef765ce09e55e678e6ba7ce2952baab37853f55
SaharOladi <sahar.olady@gmail.com>
2020-12-27 11:55:34 +0330

    Add product and slider adapter.

---
+++ app/src/main/java/com/example/digikala/ProductAdapter.java
@@ -0,0 +1,148 @@
+package com.example.digikala;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.bumptech.glide.Glide;
+import com.example.digikala.model.ImagesItem;
+import com.example.digikala.model.ProductsItem;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+public class ProductAdapter extends RecyclerView.Adapter<ProductAdapter.RecyclerHolder> implements Filterable {
+
+    private Context mContext;
+    private List<ProductsItem> mProductsItem;
+    private List<ProductsItem> mSearchProductsItem;
+
+    public List<ProductsItem> getProductsItem() {
+        return mProductsItem;
+    }
+
+    public void setProductsItem(List<ProductsItem> productsItem) {
+        mProductsItem = productsItem;
+        if (productsItem != null)
+            this.mSearchProductsItem = new ArrayList<>(productsItem);
+        notifyDataSetChanged();
+    }
+
+    public ProductAdapter(Context context, List<ProductsItem> productsItem) {
+        mContext = context;
+        mProductsItem = productsItem;
+        mSearchProductsItem = new ArrayList<>(productsItem);
+    }
+
+    @NonNull
+    @Override
+    public RecyclerHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext)
+                .inflate(R.layout.product_view_holder, parent, false);
+
+        return new RecyclerHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerHolder holder, int position) {
+        ProductsItem productItem = mProductsItem.get(position);
+        holder.bindProduct(productItem);
+
+        holder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+//                ((AppCompatActivity) mContext).getSupportFragmentManager().beginTransaction()
+//                        .replace(R.id.fragment_container, ProductDetailFragment.newInstance(productItem))
+//                        .commit();
+
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return mProductsItem.size();
+    }
+
+
+    @Override
+    public Filter getFilter() {
+        return new Filter() {
+            @Override
+            protected FilterResults performFiltering(CharSequence charSequence) {
+                List<ProductsItem> filteredList = new ArrayList<>();
+
+                if (charSequence.toString().isEmpty()) {
+                    filteredList.addAll(mSearchProductsItem);
+                } else {
+                    for (ProductsItem productsItem : mSearchProductsItem) {
+                        if (productsItem.getName().toLowerCase().trim().contains(charSequence.toString().toLowerCase().trim())) {
+                            filteredList.add(productsItem);
+                        }
+                    }
+                }
+                FilterResults results = new FilterResults();
+                results.values = filteredList;
+
+                return results;
+            }
+
+            @Override
+            protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+                if (mProductsItem != null)
+                    mProductsItem.clear();
+                if (filterResults.values != null)
+                    mProductsItem.addAll((Collection<? extends ProductsItem>) filterResults.values);
+                notifyDataSetChanged();
+            }
+        };
+    }
+
+
+    public class RecyclerHolder extends RecyclerView.ViewHolder {
+
+        private TextView mName, mPrice;
+        private ImageView mImage;
+        private View mItemView;
+
+
+        public RecyclerHolder(@NonNull View itemView) {
+            super(itemView);
+
+            findHolderViews(itemView);
+
+        }
+
+
+        private void findHolderViews(@NonNull View itemView) {
+            mName = itemView.findViewById(R.id.product_view_holder_title);
+            mPrice = itemView.findViewById(R.id.product_view_holder_price);
+            mImage = itemView.findViewById(R.id.product_view_holder_image_cover);
+
+            mItemView = itemView;
+
+        }
+
+        private void bindProduct(ProductsItem productItem) {
+            mName.setText(productItem.getName() + "");
+            mPrice.setText(productItem.getPrice() + "");
+            List<ImagesItem> imagesItems = productItem.getImages();
+            Glide.with(mItemView)
+                    .load(imagesItems.get(0).getSrc())
+                    .fitCenter()
+                    .into(mImage);
+        }
+
+    }
+
+}
---
+++ app/src/main/java/com/example/digikala/SliderAdapter.java
@@ -0,0 +1,81 @@
+package com.example.digikala;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+
+import com.bumptech.glide.Glide;
+import com.example.digikala.model.ImagesItem;
+import com.smarteist.autoimageslider.SliderViewAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class SliderAdapter extends SliderViewAdapter<SliderAdapter.SliderAdapterVH> {
+
+    private Context mContext;
+    private List<ImagesItem> mImagesItems = new ArrayList<>();
+
+    public List<ImagesItem> getImagesItems() {
+        return mImagesItems;
+    }
+
+    public void setImagesItems(List<ImagesItem> imagesItems) {
+        mImagesItems = imagesItems;
+        notifyDataSetChanged();
+    }
+
+    public SliderAdapter(Context context, List<ImagesItem> imagesItems) {
+        mContext = context;
+        mImagesItems = imagesItems;
+    }
+
+    @Override
+    public SliderAdapterVH onCreateViewHolder(ViewGroup parent) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.slider, null);
+        return new SliderAdapterVH(view);
+    }
+
+    @Override
+    public void onBindViewHolder(SliderAdapterVH viewHolder, final int position) {
+
+        ImagesItem imageItem = mImagesItems.get(position);
+        viewHolder.bindImageItem(viewHolder, imageItem);
+    }
+
+    @Override
+    public int getCount() {
+        return mImagesItems.size();
+    }
+
+    public class SliderAdapterVH extends SliderViewAdapter.ViewHolder {
+
+        private ImageView imageViewBackground;
+        private ImagesItem mImageItem;
+        private View mItemView;
+
+        public SliderAdapterVH(View itemView) {
+            super(itemView);
+            findHolderViews(itemView);
+        }
+
+        private void findHolderViews(View itemView) {
+            imageViewBackground = itemView.findViewById(R.id.iv_auto_image_slider);
+            mItemView = itemView;
+        }
+
+        private void bindImageItem(SliderAdapterVH holder, ImagesItem image) {
+            mImageItem = image;
+
+            Glide.with(mItemView)
+                    .load(image.getSrc())
+                    .fitCenter()
+                    .into(imageViewBackground);
+
+        }
+
+    }
+
+}
