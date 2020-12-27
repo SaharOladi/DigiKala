0c9bfe0e2dbbfe778999ac1018ba085b5bcca351
SaharOladi <sahar.olady@gmail.com>
2020-12-27 11:35:02 +0330

    set list of Items to repository.

--- app/src/main/java/com/example/digikala/activity/SplashActivity.java
+++ app/src/main/java/com/example/digikala/activity/SplashActivity.java
@@ -11,40 +11,46 @@ import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.Handler;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.airbnb.lottie.LottieAnimationView;
 import com.example.digikala.R;
+import com.example.digikala.model.ProductsItem;
 import com.example.digikala.network.NetworkGetter;
+import com.example.digikala.repository.Repository;
 import com.google.android.material.snackbar.Snackbar;
+
+import java.util.List;
 
 public class SplashActivity extends AppCompatActivity implements NetworkGetter.NetworkState {
 
-    public static final int DELAY_MILLIS = 3000;
+    public static final int DELAY_MILLIS = 5000;
+    public static final String TAG = "MySplashActivity";
 
     private LottieAnimationView mAnim;
     private Runnable mSplashRunnable;
     private Snackbar mSnackBar;
     private Handler splashHandler;
 
+    private Repository mRepository;
+    private NetworkGetter mNetworkGetter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
 
+        mNetworkGetter = new NetworkGetter();
+        mRepository = new Repository(SplashActivity.this);
+        RequestItemFromInternet();
+
         initialAnimation();
         splashHandler = new Handler();
         mSplashRunnable = initialRunnable();
         startMainActivity(mSplashRunnable);
-    }
 
-    private void startMainActivity(Runnable runnable) {
-        if (isNetworkAvailable())
-            splashHandler.postDelayed(runnable, DELAY_MILLIS);
-        else {
-            showSnackBar();
-        }
     }
 
     private void initialAnimation() {
@@ -68,23 +74,17 @@ public class SplashActivity extends AppCompatActivity implements NetworkGetter.N
         return activeNetworkInfo != null;
     }
 
+    private void startMainActivity(Runnable runnable) {
+        if (isNetworkAvailable()) {
+            splashHandler.postDelayed(runnable, DELAY_MILLIS);
+        } else {
+            showSnackBar();
+        }
+    }
+
     private void showSnackBar() {
-        mSnackBar = Snackbar.make(findViewById(R.id.anim_splash),
-                getString(R.string.connection_fail),
-                Snackbar.LENGTH_INDEFINITE);
-        mSnackBar.setBackgroundTint(Color.WHITE);
-        View snackBarLayout = mSnackBar.getView();
-        TextView textView = snackBarLayout.findViewById(R.id.snackbar_text);
-
-        textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_retry_foreground,
-                0,
-                0,
-                0);
-
-        textView.setText(R.string.connection_fail);
-        textView.setTextColor(Color.BLACK);
-        Typeface typeface = ResourcesCompat.getFont(SplashActivity.this, R.font.iran_yekan);
-        textView.setTypeface(typeface);
+        View snackBarLayout = initialSnackBar();
+        TextView textView = initialTextViewSnackBar(snackBarLayout);
 
         ViewCompat.setLayoutDirection(mSnackBar.getView(), ViewCompat.LAYOUT_DIRECTION_RTL);
         textView.setOnClickListener(new View.OnClickListener() {
@@ -101,8 +101,68 @@ public class SplashActivity extends AppCompatActivity implements NetworkGetter.N
         mAnim.pauseAnimation();
     }
 
+    private TextView initialTextViewSnackBar(View snackBarLayout) {
+        TextView textView = snackBarLayout.findViewById(R.id.snackbar_text);
+        textView.setCompoundDrawablesWithIntrinsicBounds(R.drawable.ic_retry_foreground,
+                0,
+                0,
+                0);
+        textView.setText(R.string.connection_fail);
+        textView.setTextColor(Color.BLACK);
+        Typeface typeface = ResourcesCompat.getFont(SplashActivity.this, R.font.iran_yekan);
+        textView.setTypeface(typeface);
+        return textView;
+    }
+
+    private View initialSnackBar() {
+        mSnackBar = Snackbar.make(findViewById(R.id.anim_splash),
+                getString(R.string.connection_fail),
+                Snackbar.LENGTH_INDEFINITE);
+        mSnackBar.setBackgroundTint(Color.WHITE);
+        return mSnackBar.getView();
+    }
+
+    private void RequestItemFromInternet() {
+        mNetworkGetter.fetchSingleProduct(608, new NetworkGetter.SingleCallbacks() {
+            @Override
+            public void onItemResponse(ProductsItem item) {
+                mRepository.setSingleProductsItem(item);
+                Log.d(TAG, "onItemResponse: "+mRepository.getSingleProductsItem().toString());
+            }
+        });
+        mNetworkGetter.fetchAllProductItemsAsync(new NetworkGetter.Callbacks() {
+            @Override
+            public void onItemResponse(List<ProductsItem> items) {
+                mRepository.setAllProductsItems(items);
+                Log.d(TAG, "onItemResponse: "+mRepository.getAllProductsItems().size());
+            }
+        });
+        mNetworkGetter.fetchMostVisitedProducts(1, new NetworkGetter.Callbacks() {
+            @Override
+            public void onItemResponse(List<ProductsItem> items) {
+                mRepository.setMostVisitedProductsItems(items);
+                Log.d(TAG, "onItemResponse: "+mRepository.getMostVisitedProductsItems().size());
+            }
+        });
+        mNetworkGetter.fetchRatedProducts(1, new NetworkGetter.Callbacks() {
+            @Override
+            public void onItemResponse(List<ProductsItem> items) {
+                mRepository.setRatedProductsItems(items);
+            }
+        });
+        mNetworkGetter.fetchRecentProducts(1, new NetworkGetter.Callbacks() {
+            @Override
+            public void onItemResponse(List<ProductsItem> items) {
+                mRepository.setRecentProductsItems(items);
+                Log.d(TAG, "onItemResponse: "+mRepository.getRecentProductsItems().size());
+            }
+        });
+    }
+
     @Override
     public void onFailureCalled() {
         showSnackBar();
     }
+
+
 }
