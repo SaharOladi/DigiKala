f373c3fd3270015aa5090a1ef6f2c13653f3557f
SaharOladi <sahar.olady@gmail.com>
2020-12-27 09:34:01 +0330

    Add font, navigation menu, and empty fragment for user, category, home and shopping.

--- app/src/main/java/com/example/digikala/activity/MainActivity.java
+++ app/src/main/java/com/example/digikala/activity/MainActivity.java
@@ -1,21 +1,91 @@
 package com.example.digikala.activity;
 
-import androidx.fragment.app.Fragment;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 
+import com.example.digikala.CategoryFragment;
+import com.example.digikala.HomeFragment;
 import com.example.digikala.R;
+import com.example.digikala.ShoppingFragment;
+import com.example.digikala.UserFragment;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
-public class MainActivity extends SingleFragmentActivity {
+public class MainActivity extends AppCompatActivity {
 
-    @Override
-    public Fragment createFragment() {
-        return null;
+    public static final String TAG = "MainActivity";
+    private BottomNavigationView mBottomNavigationView;
+
+    public static Intent newIntent(Context context) {
+        return new Intent(context, MainActivity.class);
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        findViews();
+
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction().
+                    add(R.id.fragment_container_main_activity, HomeFragment.newInstance())
+                    .commit();
+        }
+
+        moveNavigationButton();
     }
+
+    private void findViews() {
+        mBottomNavigationView = findViewById(R.id.navigation_button);
+    }
+
+
+    private void moveNavigationButton() {
+        mBottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch (item.getItemId()) {
+                    case R.id.navigation_shoppingBag:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container_main_activity,
+                                        ShoppingFragment.newInstance())
+                                .hide(HomeFragment.newInstance())
+                                .commit();
+                        return true;
+                    case R.id.navigation_profile:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container_main_activity,
+                                        UserFragment.newInstance())
+                                .hide(HomeFragment.newInstance())
+                                .commit();
+                        return true;
+                    case R.id.navigation_category:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container_main_activity,
+                                        CategoryFragment.newInstance())
+                                .hide(HomeFragment.newInstance())
+
+                                .commit();
+                        return true;
+
+                    case R.id.navigation_home:
+                        getSupportFragmentManager().beginTransaction()
+                                .replace(R.id.fragment_container_main_activity,
+                                        HomeFragment.newInstance())
+                                .hide(HomeFragment.newInstance())
+
+                                .commit();
+                        return true;
+                    default:
+                        throw new IllegalStateException("Unexpected value: " + item.getItemId());
+                }
+            }
+        });
+    }
+
 }
